f2ce6a75ded0283f05af0ba4c3d67148f690d12d
Brian Kahn <brian.kahn@noaa.gov>
2023-09-08 14:48:09 -0700

    split ProcessRSNdata.job into ProcessCTDdata.sh and ProcessBOTPTdata.sh, made crontab.noCTD

--- api/cleanup.sh
+++ api/cleanup.sh
@@ -3,15 +3,17 @@
 
 #Email="brian.kahn@noaa.gov william.w.chadwick@gmail.com andy.lau@noaa.gov"
 Email="brian.kahn@noaa.gov"
-# days to check - use parm $1 if present
-days=${1:-14}
-#exclude="2020-08-15 2020-08-16 2020-08-17 2020-08-18 2020-08-19 2020-08-20"
+# how many days to check - use command line parameter $1 if present else two weeks
+checkdays=${1:-14}
+# exclude certain days that do not and never will have data
+# exclude="2020-08-15 2020-08-16 2020-08-17 2020-08-18 2020-08-19 2020-08-20"
 exclude=""
 
 base=$(basename $0 .sh)
 dir=$(dirname $0)
 cd $dir
 
+days=$checkdays
 ( while [ $days -gt 0 ]; do
     day=$(date -u -d "$days days ago" '+%Y-%m-%d')
     if echo "$exclude" | grep -v -q $day; then
@@ -21,7 +23,7 @@ cd $dir
   done
 
   # remove segments older than days
-  find segments/ -type f -mtime +$days -delete
+  find segments/ -type f -mtime +$checkdays -delete
 ) >& $base.out
 
 if [ -n "$Email" -a -s "$base.out" ]; then 
